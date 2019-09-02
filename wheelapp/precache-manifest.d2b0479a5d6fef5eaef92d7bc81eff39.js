139c5157425e71fe068e6fc0ffb188bf61fb7c0b
Viktor Shmer <viktorshmer@gmail.com>
2019-09-02 16:11:39 +0700

    новое приложение

---
+++ wheelapp/precache-manifest.d2b0479a5d6fef5eaef92d7bc81eff39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5706413b7fb5a1b6d58875bd2ebb19fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82d760e1af2d2ecc569e",
+    "url": "/static/css/main.0a0597c3.chunk.css"
+  },
+  {
+    "revision": "fffaab3b26fb281f84fc",
+    "url": "/static/js/2.71edaf21.chunk.js"
+  },
+  {
+    "revision": "82d760e1af2d2ecc569e",
+    "url": "/static/js/main.f49aec6a.chunk.js"
+  },
+  {
+    "revision": "6c1ad29c1e8662cff8c0",
+    "url": "/static/js/runtime~main.373b8803.js"
+  },
+  {
+    "revision": "11803af23f9b0faa54138cde36e1ec8e",
+    "url": "/static/media/arrow.11803af2.svg"
+  },
+  {
+    "revision": "403f479e9f070389c9ccf98b0ae26130",
+    "url": "/static/media/wheel-min-def.403f479e.svg"
+  },
+  {
+    "revision": "7c07e0d62969e233ab7698b0e52e3ef5",
+    "url": "/static/media/wheel-min-win.7c07e0d6.svg"
+  }
+]);
