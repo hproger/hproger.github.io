6f25707c00e04f1c15f035d338e7f87419230f33
Viktor Shmer <viktorshmer@gmail.com>
2019-11-11 00:41:05 +0700

    Задачка сбербанк

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2b0479a5d6fef5eaef92d7bc81eff39.js"
+  "/precache-manifest.c872fd8cd5a4fcd803bad4ee8a72cdee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
